05d87ab4966e1f824f0d2a25209ca759bf337c3b
jeevac <cjeeva03>
2025-11-09 13:47:31 +0530

    Python Flask CI/CD - test 6

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,7 @@ COPY flask-ci-cd/requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY flask-ci-cd/app/ ./app/
 COPY flask-ci-cd/tests/ ./tests/
+WORKDIR /home/appuser/app
 EXPOSE 5000
-CMD ["python", "-m", "app.main"]
+# CMD ["python", "-m", "app.main"]
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
--- flask-ci-cd/app/main.py
+++ flask-ci-cd/app/main.py
@@ -1,15 +1,15 @@
 from flask import Flask, jsonify
 
-def create_app():
-    app = Flask(__name__)
-    
-    @app.route("/health")
-    def health():
-        return jsonify(status="ok")
-    @app.route("/hello/<name>")
-    def hello(name: str):
-        return jsonify(message=f"Hello, {name}!")
-    return app
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+
+@app.route("/health")
+def health():
+    return jsonify(status="ok")
+@app.route("/hello/<name>")
+def hello(name: str):
+    return jsonify(message=f"Hello, {name}!")
 
 if __name__ == "__main__":
-    create_app().run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
